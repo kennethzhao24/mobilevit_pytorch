672de42c8ae06dd1710f4e0d1201407d9f3037af
Kenneth Zhao <89117188+kennethzhao24@users.noreply.github.com>
2022-05-13 18:06:02 +0800

    Add files via upload

---
+++ weights/download.sh
@@ -0,0 +1,5 @@
+wget https://docs-assets.developer.apple.com/ml-research/models/cvnets/classification/mobilevit_xxs.pt
+wget https://docs-assets.developer.apple.com/ml-research/models/cvnets/classification/mobilevit_xs.pt
+wget https://docs-assets.developer.apple.com/ml-research/models/cvnets/classification/mobilevit_s.pt
+wget https://docs-assets.developer.apple.com/ml-research/models/cvnets/classification/mobilenetv2_1.0.pt
+wget https://docs-assets.developer.apple.com/ml-research/models/cvnets/classification/resnet50.pt
